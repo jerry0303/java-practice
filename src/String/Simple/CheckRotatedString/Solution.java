89536fed90fae52749da885f21f7060803c14d25
jerry0303 <vaibhav24.sharma@gmail.com>
2024-03-06 10:30:00 +0530

    Simple

---
+++ src/String/Simple/CheckRotatedString/Solution.java
@@ -0,0 +1,20 @@
+package String.Simple.CheckRotatedString;
+
+class Solution {
+    public static boolean rotateString(String s, String goal) {
+
+        if(s.length()!= goal.length())
+            return false;
+
+        String txt = s.concat(s);
+        if(txt.indexOf(goal)!=-1)
+            return true;
+        else
+            return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(rotateString("abcd", "cdab"));
+    }
+}
+
